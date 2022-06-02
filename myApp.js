f025c74b3dffcd33eab00ecebe578f3fff30dd8e
S Venkatesh <90242936+xvenkyx@users.noreply.github.com>
2022-06-02 10:08:33 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,4 +1,11 @@
-console.log("Hello World")
+var express = require('express');
+var app = express();
+
+console.log("Hello World");
+
+app.get('/', (req,res) => {
+  res.send('Hello Express')
+})
 
 
 
